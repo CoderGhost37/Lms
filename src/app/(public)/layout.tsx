7db13cdd141e5f720a51c93abe7e273906cc41a0
coderghost37 <coderghost37@gmail.com>
2025-08-17 16:23:02 +0530

    feat: public courses page

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -4,7 +4,7 @@ export default function PublicLayout({ children }: { children: React.ReactNode }
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-1 container mx-auto px-4 sm:px-6 lg:px-8">{children}</main>
+      <main className="flex-1 container mx-auto px-4 sm:px-6 lg:px-8 pb-32">{children}</main>
     </div>
   )
 }
