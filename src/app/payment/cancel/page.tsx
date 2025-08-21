80e36685caa9e5ac8aa2d94290ee95fc88535718
CoderGhost37 <coderghost37@gmail.com>
2025-08-21 10:29:54 +0530

    feat: add payment cancellation and success pages

---
+++ src/app/payment/cancel/page.tsx
@@ -0,0 +1,29 @@
+import { ArrowLeft, XIcon } from 'lucide-react'
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+
+export default function PaymentCancelledPage() {
+  return (
+    <div className="w-full min-h-screen flex flex-1 justify-center items-center">
+      <Card className="w-[350px]">
+        <CardContent>
+          <div className="w-full flex items-center justify-center">
+            <XIcon className="size-12 p-2 bg-red-500/30 text-red-500 rounded-full" />
+          </div>
+          <div className="mt-3 text-center sm:mt-5 w-full">
+            <h2 className="text-xl font-semibold">Payment Cancelled</h2>
+            <p className="text-sm tracking-tight mt-2 text-muted-foreground text-balance">
+              No worries, you won't be charged. Please try again.
+            </p>
+
+            <Link href="/" className={buttonVariants({ className: 'w-full mt-5' })}>
+              <ArrowLeft className="size-4" />
+              Go back to Homepage
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
