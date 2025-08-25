1cfcbab8e1231f118b816515c3945b3e78aee6e7
CoderGhost37 <coderghost37@gmail.com>
2025-08-26 00:46:58 +0530

    fix: prisma vercel error

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -10,7 +11,14 @@ const nextConfig: NextConfig = {
         port: ''
       }
     ]
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
 };
 
 export default nextConfig;
